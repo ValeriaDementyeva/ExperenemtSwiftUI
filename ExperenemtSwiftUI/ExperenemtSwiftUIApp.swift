b4a7b20825cb29413c7e46e81974f3e71cc0d1c4
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-09-18 21:06:14 +0300

    Initial Commit

---
+++ ExperenemtSwiftUI/ExperenemtSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperenemtSwiftUIApp.swift
+//  ExperenemtSwiftUI
+//
+//  Created by Валерия Дементьева on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExperenemtSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
